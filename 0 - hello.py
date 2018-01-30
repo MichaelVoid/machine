965b1fce4b95d6f9e7fb88900bdffeb3ea8971a8
MichaelVoid <jimmyrevenga@gmail.com>
2018-01-30 15:24:29 +0100

    Add existing file

---
+++ 0 - hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 14:13:38 2018
+
+@author: mig
+"""
+
+import Tkinter as tk
+
+root = tk.Tk()
+theLabel = tk.Label(root,text="Hello")
+theLabel.pack()    # where to put it
+root.mainloop()
---
+++ 1 - frame.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 14:13:38 2018
+# how to make a frame
+https://www.youtube.com/watch?v=RTM9tiV_HoY&index=2&list=PL6gx4Cwl9DGBwibXFtPtflztSNPGuIB_d
+@author: mig
+"""
+
+import Tkinter as tk
+
+root = tk.Tk()
+topFrame = Frame(root)
+topFrame.pack()
+
+
+root.mainloop()
